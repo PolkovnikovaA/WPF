ef51892b3421a94aa40fbdfc40e8d5f75f202888
PolkovnikovaA <104072166+PolkovnikovaA@users.noreply.github.com>
2022-10-17 15:49:24 +0300

    Update Delet_Vibr.xaml.cs

--- WpfApp1/Delet_Vibr.xaml.cs
+++ WpfApp1/Delet_Vibr.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +24,32 @@ namespace WpfApp1
         public Delet_Vibr()
         {
             InitializeComponent();
+        }
+        public DataTable Select(string selectSQL) // функция подключения к базе данных и обработка запросов
+        {
+            DataTable dataTable = new DataTable("dataBase"); // создаём таблицу в приложении
+                                                             // подключаемся к базе данных
+            SqlConnection sqlConnection = new SqlConnection("server=ngknn.ru;Trusted_Connection=No;DataBase=Truba;User=33П;PWD=12357");
+            sqlConnection.Open(); // открываем базу данных
+            SqlCommand sqlCommand = sqlConnection.CreateCommand(); // создаём команду
+            sqlCommand.CommandText = selectSQL; // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);
+            sqlConnection.Close(); // возращаем таблицу с результатом
+            return dataTable;
+        }
+
+
+        public string Dell = "";
+
+        private void Dell_Click(object sender, RoutedEventArgs e)
+        {
+            Dell = Deletee.Text;
+            DataTable dt_user = Select("Delete Emission   WHERE id_emission = ('" + Deletee.Text + "')");
+            MainWindow win_gl = new MainWindow();
+            win_gl.Show();
+
+            Close();
         }
     }
 }
