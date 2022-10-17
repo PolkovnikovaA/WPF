ce665d843ee4e4461393478ec202abca3c5c4636
PolkovnikovaA <104072166+PolkovnikovaA@users.noreply.github.com>
2022-10-17 15:46:12 +0300

    Update Add.xaml.cs

--- WpfApp1/Add.xaml.cs
+++ WpfApp1/Add.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,10 +26,45 @@ namespace WpfApp1
             InitializeComponent();
         }
 
-        private void Otm_Click(object sender, RoutedEventArgs e)
+        public DataTable Select(string selectSQL) // функция подключения к базе данных и обработка запросов
         {
-            Hide();
+            DataTable dataTable = new DataTable("dataBase"); // создаём таблицу в приложении
+                                                             // подключаемся к базе данных
+            SqlConnection sqlConnection = new SqlConnection("server=ngknn.ru;Trusted_Connection=No;DataBase=Truba;User=33П;PWD=12357");
+            sqlConnection.Open(); // открываем базу данных
+            SqlCommand sqlCommand = sqlConnection.CreateCommand(); // создаём команду
+            sqlCommand.CommandText = selectSQL; // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);
+            sqlConnection.Close(); // возращаем таблицу с результатом
+            return dataTable;
         }
 
+
+
+        private void Otm_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        public string Ist = "";
+        public string Address = "";
+
+        private void Add2_Click(object sender, RoutedEventArgs e)
+        {
+            Ist = Ist1.Text;
+            Address = Address1.Text;
+            DataTable dt_user = Select("insert into Source (name, address) values('" + Ist1.Text + "','" + Address1.Text + "')");
+            MainWindow win_gl = new MainWindow();
+            win_gl.Show(); 
+
+
+
+
+
+
+
+            Close();
+        }
     }
 }
