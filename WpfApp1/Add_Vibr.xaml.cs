7bf4ca846afb86771dbe1fbeb85c9baacfe26cbc
PolkovnikovaA <104072166+PolkovnikovaA@users.noreply.github.com>
2022-10-17 15:47:21 +0300

    Update Add_Vibr.xaml.cs

--- WpfApp1/Add_Vibr.xaml.cs
+++ WpfApp1/Add_Vibr.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +24,38 @@ namespace WpfApp1
         public Add_Vibr()
         {
             InitializeComponent();
+        }
+        public DataTable Select(string selectSQL) // функция подключения к базе данных и обработка запросов
+        {
+            DataTable dataTable = new DataTable("dataBase"); // создаём таблицу в приложении
+                                                             // подключаемся к базе данных
+            SqlConnection sqlConnection = new SqlConnection("server=ngknn.ru;Trusted_Connection=No;DataBase=Truba;User=33П;PWD=12357");
+            sqlConnection.Open(); // открываем базу данных
+            SqlCommand sqlCommand = sqlConnection.CreateCommand(); // создаём команду
+            sqlCommand.CommandText = selectSQL; // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);
+            sqlConnection.Close(); // возращаем таблицу с результатом
+            return dataTable;
+        }
+
+
+        public string Istok = "";
+        public string Kol = "";
+        public string Kom = "";
+        public string Dat = "";
+
+        private void Add3_Click(object sender, RoutedEventArgs e)
+        {
+            Istok = Istok1.Text;
+            Kol = Koll.Text;
+            Kom = Koment.Text;
+            Dat = date.Text;
+            DataTable dt_user = Select("insert into Emission (id_source, count, text, date) values('" + Istok1.Text + "','" + Koll.Text + "','" + Koment.Text + "','" + date.Text + "')");
+            MainWindow win_gl = new MainWindow();
+            win_gl.Show();
+
+            Close();
         }
     }
 }
