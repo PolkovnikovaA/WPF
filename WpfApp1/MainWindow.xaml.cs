7082ce689878a438f71008499702d5a077600c57
Nastya <21nastya.chernaya19@gmail.com>
2022-10-17 09:08:42 +0300

    vivod table

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -28,7 +28,6 @@ namespace WpfApp1
                                                              // подключаемся к базе данных
             SqlConnection sqlConnection = new SqlConnection("server=ngknn.ru;Trusted_Connection=No;DataBase=Man_Sor_V_A;User=33П;PWD=12357");
             sqlConnection.Open(); // открываем базу данных
-            string cmd = "SELECT * FROM Source";
             SqlCommand sqlCommand = sqlConnection.CreateCommand(); // создаём команду
             sqlCommand.CommandText = selectSQL; // присваиваем команде текст
             SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
@@ -44,33 +43,101 @@ namespace WpfApp1
             {
             
                 connection.Open();
-                string cmd = "SELECT * FROM Source"; // Из какой таблицы нужен вывод 
-                SqlCommand createCommand = new SqlCommand(cmd, connection);
+                SqlCommand createCommand = new SqlCommand("SELECT max(id_source) FROM Source", connection);
+
+
+                //string cmd = "SELECT * FROM Source"; // Из какой таблицы нужен вывод 
+                //SqlCommand createCommand = new SqlCommand(cmd, connection);
                 int n = Convert.ToInt32(createCommand.ExecuteScalar().ToString());
-                int[] id = new int[n];
+                int[] id_source = new int[n];
                 string[] name = new string[n];
-                string[] Address = new string[n];
+                string[] address = new string[n];
 
                 for (int i = 1; i <= n; i++)
                 {
-                    SqlCommand command1 = new SqlCommand("select id_source from Source Where id_source=" + i + "", connection);
-                    if (command1.ExecuteScalar() is null)
+                    SqlCommand createCommand1 = new SqlCommand("select id_source from Source Where id_source=" + i + "", connection);
+                    if (createCommand1.ExecuteScalar() is null)
                     {
 
                     }
                     else 
                     {
-                        id[i - 1] = Convert.ToInt32(command1.ExecuteScalar().ToString());
-                        SqlCommand command2 = new SqlCommand("select name from Source Where id_source=" + i + "", connection);
-                        name[i-1] = Convert.ToString(command2.ExecuteScalar().ToString());
-                        SqlCommand command3 = new SqlCommand("select name from Source Where id_source=" + i + "", connection);
-                        Address[i-1] = Convert.ToString(command3.ExecuteScalar().ToString());
+                        id_source[i - 1] = Convert.ToInt32(createCommand1.ExecuteScalar().ToString());
+                        SqlCommand createCommand2 = new SqlCommand("select name from Source Where id_source=" + i + "", connection);
+                        name[i-1] = Convert.ToString(createCommand2.ExecuteScalar().ToString());
+                        SqlCommand createCommand3 = new SqlCommand("select address from Source Where id_source=" + i + "", connection);
+                        address[i-1] = Convert.ToString(createCommand3.ExecuteScalar().ToString());
                     }
                 }
-                List<SourceGrid> istlist = new List<SourceGrid>
+                List<Istlist> istlist = new List<Istlist>
                 {
 
                 };
+                for (int i = 1; i <= n; i++)
+                {
+                    if (address[i-1] == null)
+                    {
+
+                    }
+                    else
+                    {
+                        istlist.Add(new Istlist { id_source = i, name = name[i - 1], address = address[i - 1] });
+                    }
+                }
+                table_1.ItemsSource = istlist;
+
+            }
+
+            using (SqlConnection connection = new SqlConnection("server=ngknn.ru;Trusted_Connection=No;DataBase=Truba;User=33П;PWD=12357"))
+            {
+
+                connection.Open();
+                SqlCommand createCommand = new SqlCommand("SELECT max(id_emission) FROM Emission", connection);
+
+
+                //string cmd = "SELECT * FROM Source"; // Из какой таблицы нужен вывод 
+                //SqlCommand createCommand = new SqlCommand(cmd, connection);
+                int n = Convert.ToInt32(createCommand.ExecuteScalar().ToString());
+                int[] id_emission = new int[n];
+                int[] id_source = new int[n];
+                float[] count = new float[n];
+                string[] text = new string[n];
+                string[] date = new string[n];
+
+                for (int i = 1; i <= n; i++)
+                {
+                    SqlCommand createCommand1 = new SqlCommand("select id_source from Emission Where id_emission=" + i + "", connection);
+                    if (createCommand1.ExecuteScalar() is null)
+                    {
+
+                    }
+                    else
+                    {
+                        id_emission[i - 1] = Convert.ToInt32(createCommand1.ExecuteScalar().ToString());
+                        SqlCommand createCommand2 = new SqlCommand("select count from Emission Where id_emission=" + i + "", connection);
+                        count[i - 1] = float.Parse(createCommand2.ExecuteScalar().ToString());
+                        SqlCommand createCommand3 = new SqlCommand("select text from Emission Where id_emission=" + i + "", connection);
+                        text[i - 1] = Convert.ToString(createCommand3.ExecuteScalar().ToString());
+                        SqlCommand createCommand4 = new SqlCommand("select date from Emission Where id_emission=" + i + "", connection);
+                        date[i - 1] = Convert.ToString(createCommand4.ExecuteScalar().ToString());
+                    }
+                }
+                List<Vibrlist> vibrlist = new List<Vibrlist>
+                {
+
+                };
+                for (int i = 1; i <= n; i++)
+                {
+                    if (date[i - 1] == null)
+                    {
+
+                    }
+                    else
+                    {
+                        vibrlist.Add(new Vibrlist { id_emission = i, id_source = id_source[i-1], count = count[i - 1], text = text[i - 1], date = date[i - 1] });
+                    }
+                }
+                table_2.ItemsSource = vibrlist;
 
             }
 
@@ -123,6 +190,22 @@ namespace WpfApp1
         {
             Delet_Vibr delet_Vibr = new Delet_Vibr();
             delet_Vibr.Show();
+        }
+
+        internal class Istlist
+        {
+            public int id_source { get; set; }
+            public string name { get; set; }
+            public string address { get; set; }
+        }
+
+        internal class Vibrlist
+        {
+            public int id_emission { get; set; }
+            public int id_source { get; set; }
+            public float count { get; set; }
+            public string text { get; set; }
+            public string date { get; set; }
         }
     }
 }
