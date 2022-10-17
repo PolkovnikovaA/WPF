439e3a8d0bb0ed768e7574267b5e0ef1f7e156b2
PolkovnikovaA <104072166+PolkovnikovaA@users.noreply.github.com>
2022-10-17 15:51:34 +0300

    Update Red_Ist.xaml.cs

--- WpfApp1/Red_Ist.xaml.cs
+++ WpfApp1/Red_Ist.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +26,38 @@ namespace WpfApp1
             InitializeComponent();
         }
 
-        
+
+        public DataTable Select(string selectSQL) // функция подключения к базе данных и обработка запросов
+        {
+            DataTable dataTable = new DataTable("dataBase"); // создаём таблицу в приложении
+                                                             // подключаемся к базе данных
+            SqlConnection sqlConnection = new SqlConnection("server=ngknn.ru;Trusted_Connection=No;DataBase=Truba;User=33П;PWD=12357");
+            sqlConnection.Open(); // открываем базу данных
+
+            SqlCommand sqlCommand = sqlConnection.CreateCommand(); // создаём команду
+            sqlCommand.CommandText = selectSQL; // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);
+            sqlConnection.Close(); // возращаем таблицу с результатом
+            return dataTable;
+        }
+
+
+        public string IDD = "";
+        public string Namee = "";
+        public string Adres = "";
+
+        private void Redect_Click(object sender, RoutedEventArgs e)
+        {
+            IDD = id.Text;
+            Namee = namee.Text;
+            Adres = adres.Text;
+            DataTable dt_user = Select("Update Source Set name = ('" + namee.Text + "'), address = ('" + adres.Text + "')  WHERE id_source = ('" + id.Text + "')");
+            MainWindow win_gl = new MainWindow();
+            win_gl.Show();
+
+            Close();
+        }
+
     }
 }
